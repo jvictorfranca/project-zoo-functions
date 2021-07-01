97f8e806b39107d1b582e3bfce626f6c94c80ba2
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-07-01 01:24:55 -0300

    arruma o linter pela segunda vez

--- src/zoo.js
+++ src/zoo.js
@@ -83,14 +83,6 @@ function calculateEntry(entrants) {
   return answer;
 }
 
-// function fixOrder(string, action) {
-//   const obj = {};
-//   string.forEach((value) => {
-//     obj[value] = action(value);
-//   });
-//   return obj;
-// }
-
 // function getAnimalMapCase1(string) { // if (options === undefined)
 //   return species.filter((specie) => specie.location === string)
 //     .map((specie) => specie.name);
@@ -128,30 +120,39 @@ function calculateEntry(entrants) {
 //   });
 // }
 
-// eslint-disable-next-line complexity
+// function getAnimalMap(options) {
+// seu código aqui
+// let answer;
+// const order = ['NE', 'NW', 'SE', 'SW'];
+// if (options === undefined) {
+//   answer = fixOrder(order, getAnimalMapCase1);
+// } else {
+//   const { includeNames = false, sorted = false, sex = 'any' } = options;
+//   if (includeNames === true) {
+//     answer = fixOrder(order, getAnimalMapCase2);
+//   } if (includeNames === true && sorted === true) {
+//     answer = fixOrder(order, getAnimalMapCase3);
+//   } if (includeNames === true && sex === 'male') {
+//     answer = fixOrder(order, getAnimalMapCaseMale);
+//   } if (includeNames === true && sex === 'female') {
+//     answer = fixOrder(order, getAnimalMapCaseFemale);
+//   } if (includeNames === true && sex === 'male') {
+//     answer = fixOrder(order, getAnimalMapCaseMale);
+//   } if (includeNames === true && sex === 'female') {
+//     answer = fixOrder(order, getAnimalMapCaseFemale);
+//   }
+// }
+// return answer;
+// }
+
 function getAnimalMap(options) {
   // seu código aqui
   // let answer;
   // const order = ['NE', 'NW', 'SE', 'SW'];
-  // if (options === undefined) {
+  // if (options === undefined || options.includeNames === false) {
   //   answer = fixOrder(order, getAnimalMapCase1);
-  // } else {
-  //   const { includeNames = false, sorted = false, sex = 'any' } = options;
-  //   if (includeNames === true) {
-  //     answer = fixOrder(order, getAnimalMapCase2);
-  //   } if (includeNames === true && sorted === true) {
-  //     answer = fixOrder(order, getAnimalMapCase3);
-  //   } if (includeNames === true && sex === 'male') {
-  //     answer = fixOrder(order, getAnimalMapCaseMale);
-  //   } if (includeNames === true && sex === 'female') {
-  //     answer = fixOrder(order, getAnimalMapCaseFemale);
-  //   } if (includeNames === true && sex === 'male') {
-  //     answer = fixOrder(order, getAnimalMapCaseMale);
-  //   } if (includeNames === true && sex === 'female') {
-  //     answer = fixOrder(order, getAnimalMapCaseFemale);
-  //   }
-  // }
-  // return answer;
+  // } else { const { includeNames = false, sorted = false, sex = 'any' } = options; }
+
 }
 
 function getSchedule(dayName) {
@@ -175,6 +176,7 @@ function getSchedule(dayName) {
 }
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+
 }
 
 function increasePrices(percentage) {
