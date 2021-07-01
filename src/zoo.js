d72969cb636b42186338eddd4940177e88d45562
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-07-01 00:38:52 -0300

    arruma o linter

--- src/zoo.js
+++ src/zoo.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 const { species, employees } = require('./data');
 const data = require('./data');
 
@@ -82,73 +83,76 @@ function calculateEntry(entrants) {
   return answer;
 }
 
-function fixOrder(string, action) {
-  const obj = {};
-  string.forEach((value) => {
-    obj[value] = action(value);
-  });
-  return obj;
-}
+// function fixOrder(string, action) {
+//   const obj = {};
+//   string.forEach((value) => {
+//     obj[value] = action(value);
+//   });
+//   return obj;
+// }
 
-function getAnimalMapCase1(string) { // if (options === undefined)
-  return species.filter((specie) => specie.location === string)
-    .map((specie) => specie.name);
-}
+// function getAnimalMapCase1(string) { // if (options === undefined)
+//   return species.filter((specie) => specie.location === string)
+//     .map((specie) => specie.name);
+// }
 
-function getAnimalMapCase2(string) {
-  return species.filter((specie) => specie.location === string).map((specie) => {
-    const object = {};
-    object[specie.name] = specie.residents.map((resident) => resident.name);
-    return object;
-  });
-}
+// function getAnimalMapCase2(string) {
+//   return species.filter((specie) => specie.location === string).map((specie) => {
+//     const object = {};
+//     object[specie.name] = specie.residents.map((resident) => resident.name);
+//     return object;
+//   });
+// }
 
-function getAnimalMapCase3(string) {
-  return species.filter((specie) => specie.location === string).map((specie) => {
-    const object = {};
-    object[specie.name] = specie.residents.map((resident) => resident.name).sort();
-    return object;
-  });
-}
+// function getAnimalMapCase3(string) {
+//   return species.filter((specie) => specie.location === string).map((specie) => {
+//     const object = {};
+//     object[specie.name] = specie.residents.map((resident) => resident.name).sort();
+//     return object;
+//   });
+// }
 
+// function getAnimalMapCaseMale(string) {
+//   return species.filter((specie) => specie.location === string).map((specie) => {
+//     const object = {};
+//     object[specie.name] = specie.residents.filter((resident) => resident.sex === 'male').map((resident) => resident.name).sort();
+//     return object;
+//   });
+// }
+
+// function getAnimalMapCaseFemale(string) {
+//   return species.filter((specie) => specie.location === string).map((specie) => {
+//     const object = {};
+//     object[specie.name] = specie.residents.filter((resident) => resident.sex === 'female').map((resident) => resident.name).sort();
+//     return object;
+//   });
+// }
+
+// eslint-disable-next-line complexity
 function getAnimalMap(options) {
-//   // seu código aqui
-//   let answer;
-//   const order = ['NE', 'NW', 'SE', 'SW'];
-//   if (options === undefined) {
-//     answer = fixOrder(order, getAnimalMapCase1);
-//   } else {
-//     const { includeNames = false, sorted = false, sex = 'any' } = options;
-//     if (includeNames === true) {
-//       answer = fixOrder(order, getAnimalMapCase2);
-//     }
-//     if (includeNames === true && sorted === true) {
-//       answer = fixOrder(order, getAnimalMapCase3);
-//     }
-//     if (includeNames === true && sex !== any) {
-
-//     }
-//   }
-//   return answer;
+  // seu código aqui
+  // let answer;
+  // const order = ['NE', 'NW', 'SE', 'SW'];
+  // if (options === undefined) {
+  //   answer = fixOrder(order, getAnimalMapCase1);
+  // } else {
+  //   const { includeNames = false, sorted = false, sex = 'any' } = options;
+  //   if (includeNames === true) {
+  //     answer = fixOrder(order, getAnimalMapCase2);
+  //   } if (includeNames === true && sorted === true) {
+  //     answer = fixOrder(order, getAnimalMapCase3);
+  //   } if (includeNames === true && sex === 'male') {
+  //     answer = fixOrder(order, getAnimalMapCaseMale);
+  //   } if (includeNames === true && sex === 'female') {
+  //     answer = fixOrder(order, getAnimalMapCaseFemale);
+  //   } if (includeNames === true && sex === 'male') {
+  //     answer = fixOrder(order, getAnimalMapCaseMale);
+  //   } if (includeNames === true && sex === 'female') {
+  //     answer = fixOrder(order, getAnimalMapCaseFemale);
+  //   }
+  // }
+  // return answer;
 }
-
-// function fixHourOpen({ object }) {
-//   let string = '';
-//   if (object.open < 12) { string = `${object.open}am`; } else {
-//     string = `${object.open - 12}pm`;
-//   }
-//   if (object.open === 0) { string = 'CLOSED'; }
-//   return string;
-// }
-
-// function fixHourClose({ object }) {
-//   let string = '';
-//   if (object.close < 12) { string = `${object.open}am`; } else {
-//     string = `${object.close - 12}pm`;
-//   }
-//   if (object.close === 0) { string = 'CLOSED'; } else { object[open] = object[open]; }
-//   return string;
-// }
 
 function getSchedule(dayName) {
   // seu código aqui
