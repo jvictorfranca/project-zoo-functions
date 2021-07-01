1e812a75969d8c90ba5317af848848ca3d9c342f
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-07-01 16:37:22 -0300

    faz requisito 9 AIIIII SIMMMM

--- src/zoo.js
+++ src/zoo.js
@@ -83,76 +83,79 @@ function calculateEntry(entrants) {
   return answer;
 }
 
-// function getAnimalMapCase1(string) { // if (options === undefined)
-//   return species.filter((specie) => specie.location === string)
-//     .map((specie) => specie.name);
-// }
+//= = undefined:
+function case0(location, array) { // Funcional
+  return array.filter((specie) => specie.location === location).map((specie) => specie.name);
+}
 
-// function getAnimalMapCase2(string) {
-//   return species.filter((specie) => specie.location === string).map((specie) => {
-//     const object = {};
-//     object[specie.name] = specie.residents.map((resident) => resident.name);
-//     return object;
-//   });
-// }
+//= /= undefined: All names
+function case1(location, array) { // Funcional
+  return array.filter((specie) => specie.location === location).map((specie) => {
+    const obj = {};
+    obj[specie.name] = specie.residents.map((resident) => resident.name);
+    return obj;
+  });
+}
 
-// function getAnimalMapCase3(string) {
-//   return species.filter((specie) => specie.location === string).map((specie) => {
-//     const object = {};
-//     object[specie.name] = specie.residents.map((resident) => resident.name).sort();
-//     return object;
-//   });
-// }
+function fixOrder(string, action, array) {
+  const obj = {};
+  string.forEach((value) => {
+    obj[value] = action(value, array);
+  });
+  return obj;
+}
 
-// function getAnimalMapCaseMale(string) {
-//   return species.filter((specie) => specie.location === string).map((specie) => {
-//     const object = {};
-//     object[specie.name] = specie.residents.filter((resident) => resident.sex === 'male').map((resident) => resident.name).sort();
-//     return object;
-//   });
-// }
+function filterSex(sex, array) {
+  return array.map((specie) => {
+    const obj = { name: specie.name };
+    obj.popularity = specie.popularity;
+    obj.location = specie.location;
+    obj.residents = specie.residents.filter((resident) => resident.sex === sex);
+    return obj;
+  });
+}
 
-// function getAnimalMapCaseFemale(string) {
-//   return species.filter((specie) => specie.location === string).map((specie) => {
-//     const object = {};
-//     object[specie.name] = specie.residents.filter((resident) => resident.sex === 'female').map((resident) => resident.name).sort();
-//     return object;
-//   });
-// }
+function sortResidents(array) {
+  return array.map((specie) => {
+    const obj = { name: specie.name };
+    obj.popularity = specie.popularity;
+    obj.location = specie.location;
+    obj.residents = specie.residents
+      .sort((resident1, resident2) => (resident1.name > resident2.name ? 1 : -1));
+    return obj;
+  });
+}
 
-// function getAnimalMap(options) {
-// seu código aqui
-// let answer;
-// const order = ['NE', 'NW', 'SE', 'SW'];
-// if (options === undefined) {
-//   answer = fixOrder(order, getAnimalMapCase1);
-// } else {
-//   const { includeNames = false, sorted = false, sex = 'any' } = options;
-//   if (includeNames === true) {
-//     answer = fixOrder(order, getAnimalMapCase2);
-//   } if (includeNames === true && sorted === true) {
-//     answer = fixOrder(order, getAnimalMapCase3);
-//   } if (includeNames === true && sex === 'male') {
-//     answer = fixOrder(order, getAnimalMapCaseMale);
-//   } if (includeNames === true && sex === 'female') {
-//     answer = fixOrder(order, getAnimalMapCaseFemale);
-//   } if (includeNames === true && sex === 'male') {
-//     answer = fixOrder(order, getAnimalMapCaseMale);
-//   } if (includeNames === true && sex === 'female') {
-//     answer = fixOrder(order, getAnimalMapCaseFemale);
-//   }
-// }
-// return answer;
-// }
+const order = ['NE', 'NW', 'SE', 'SW'];
+
+function ifSex(sex, array) {
+  let answer = array;
+  if (sex === 'female' || sex === 'male') {
+    answer = filterSex(sex, array);
+  }
+  return answer;
+}
+
+function ifSort(sorted, array) {
+  let answer = array;
+  if (sorted === true) { answer = sortResidents(array); }
+
+  return answer;
+}
 
 function getAnimalMap(options) {
-  // seu código aqui
-  // let answer;
-  // const order = ['NE', 'NW', 'SE', 'SW'];
-  // if (options === undefined || options.includeNames === false) {
-  //   answer = fixOrder(order, getAnimalMapCase1);
-  // } else { const { includeNames = false, sorted = false, sex = 'any' } = options; }
+  let answer = [];
+  let array = JSON.parse(JSON.stringify(species));
+  if (options === undefined || options.includeNames !== true) {
+    answer = fixOrder(order, case0, array);
+  } else {
+    const { sorted = false, sex } = options;
 
+    array = ifSex(sex, array);
+    ifSort(sorted, array);
+    answer = fixOrder(order, case1, array);
+  }
+  return answer;
 }
 
 function getSchedule(dayName) {
